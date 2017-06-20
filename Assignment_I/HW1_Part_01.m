f42671d0b5d017de1eebba746a932d7721ac489f
Shawn <shawn@nwk--179-25.nwk.ruw.rutgers.edu>
2017-06-20 01:14:29 -0400

    Update Hw1

--- Assignment_I/HW1_Part_01.m
+++ Assignment_I/HW1_Part_01.m
@@ -67,6 +67,108 @@ new_labels = labels + 1;
 
 % Linear Discriminant
 
+% correrate_train = 0;
+% correrate_test = 0;
+% for i = 1:10
+%     % Training Set
+%     X = norm_data(Indices == i,:);
+%     Y = new_labels(Indices == i,:);
+%     len_data = length(X);
+%     train_len = double(len_data - int16(len_data/10));
+%     test_len = double(int16(len_data/10));
+%     % modeling
+%     lda = fitcdiscr(X(1:train_len,:),Y(1:train_len,:));
+%     
+%     % apply model on training 
+%     class_out_train = predict(lda,X(1:train_len,:));
+%     % compute the confusion matrix
+%     [train_lda_CM,train_grpOrder] = confusionmat(Y(1:(len_data - int16(len_data/10))),class_out_train);
+%     correrate_train = correrate_train + (train_lda_CM(1,1) + train_lda_CM(2,2))/train_len;
+%     
+%     % 1.4 Apply model on Testing Set
+%     class_out_test = predict(lda,X(train_len+1:end,:));
+%     [test_lda_CM,test_grpOrder] = confusionmat(Y(train_len+1:end),class_out_test);
+%     if size(test_lda_CM,1) == 1
+%         correrate_test = correrate_test + 1;
+%     else
+%         correrate_test = correrate_test + (test_lda_CM(1,1) + test_lda_CM(2,2))/test_len;
+%     end
+% end
+% 
+% lda_avg_cr_train = correrate_train / 10;
+% lda_avg_cr_test = correrate_test / 10;
+
+
+% % Support Vector Machine with Linear Kernel
+% 
+% correrate_train = 0;
+% correrate_test = 0;
+% for i = 1:10
+%     % Training Set
+%     X = norm_data(Indices == i,:);
+%     Y = new_labels(Indices == i,:);
+%     len_data = length(X);
+%     train_len = double(len_data - int16(len_data/10));
+%     test_len = double(int16(len_data/10));
+%     % modeling
+%     svm_l = fitcsvm(X(1:train_len,:),Y(1:train_len,:),'KernelFunction','linear');
+%     
+%     % apply model on training 
+%     class_out_train = predict(svm_l,X(1:train_len,:));
+%     % compute the confusion matrix
+%     [train_lda_CM,train_grpOrder] = confusionmat(Y(1:(len_data - int16(len_data/10))),class_out_train);
+%     correrate_train = correrate_train + (train_lda_CM(1,1) + train_lda_CM(2,2))/train_len;
+%     
+%     % 1.4 Apply model on Testing Set
+%     class_out_test = predict(svm_l,X(train_len+1:end,:));
+%     [test_lda_CM,test_grpOrder] = confusionmat(Y(train_len+1:end),class_out_test);
+%     if size(test_lda_CM,1) == 1
+%         correrate_test = correrate_test + 1;
+%     else
+%         correrate_test = correrate_test + (test_lda_CM(1,1) + test_lda_CM(2,2))/test_len;
+%     end
+% end
+% svml_avg_cr_train = correrate_train / 10;
+% svml_avg_cr_test = correrate_test / 10;
+
+
+% % Support Vector Machine with Non Linear Kernel
+%  
+
+% 
+% correrate_train = 0;
+% correrate_test = 0;
+% for i = 1:10
+%     % Training Set
+%     X = norm_data(Indices == i,:);
+%     Y = new_labels(Indices == i,:);
+%     len_data = length(X);
+%     train_len = double(len_data - int16(len_data/10));
+%     test_len = double(int16(len_data/10));
+%     % modeling
+%     svm_rbf = fitcsvm(X(1:train_len,:),Y(1:train_len,:),'KernelFunction','rbf');
+%     
+%     % apply model on training 
+%     class_out_train = predict(svm_rbf,X(1:train_len,:));
+%     % compute the confusion matrix
+%     [train_lda_CM,train_grpOrder] = confusionmat(Y(1:(len_data - int16(len_data/10))),class_out_train);
+%     correrate_train = correrate_train + (train_lda_CM(1,1) + train_lda_CM(2,2))/train_len;
+%     
+%     % 1.4 Apply model on Testing Set
+%     class_out_test = predict(svm_rbf,X(train_len+1:end,:));
+%     [test_lda_CM,test_grpOrder] = confusionmat(Y(train_len+1:end),class_out_test);
+%     if size(test_lda_CM,1) == 1
+%         correrate_test = correrate_test + 1;
+%     else
+%         correrate_test = correrate_test + (test_lda_CM(1,1) + test_lda_CM(2,2))/test_len;
+%     end
+% end
+% 
+% svmrbf_avg_cr_train = correrate_train / 10;
+% svmrbf_avg_cr_test = correrate_test / 10;
+
+% Artificial Neural Network(ANN)
+
 correrate_train = 0;
 correrate_test = 0;
 for i = 1:10
@@ -77,23 +179,31 @@ for i = 1:10
     train_len = double(len_data - int16(len_data/10));
     test_len = double(int16(len_data/10));
     % modeling
-    lda = fitcdiscr(X(1:train_len,:),Y(1:train_len,:));
-    
+    net = newff([0 1;0 1;0 1;0 1;0 1;0 1;0 1;0 1;],[5 1,1],{'tansig' 'purelin','purelin'});
+    net.trainParam.epochs = 200;
+    net = train(net,X(1:train_len,:)',Y(1:train_len,:)');
     % apply model on training 
-    class_out_train = predict(lda,X(1:train_len,:));
-    % compute the confusion matrix
-    [train_lda_CM,train_grpOrder] = confusionmat(Y(1:(len_data - int16(len_data/10))),class_out_train);
-    correrate_train = correrate_train + (train_lda_CM(1,1) + train_lda_CM(2,2))/train_len;
+    Y_pred = sim(net,X(1:train_len,:)');
+    %plot(X',Y',X',Y_pred,'o')
+    Y_pred(Y_pred > 1.5) = 2;
+    Y_pred(Y_pred < 1.5) = 1;
+    % compute the confusion matrix for train set
+    [train_ann_CM,train_grpOrder] = confusionmat(Y(1:(len_data - int16(len_data/10))),Y_pred);
+    correrate_train = correrate_train + (train_ann_CM(1,1) + train_ann_CM(2,2))/train_len;
     
-    % 1.4 Apply model on Testing Set
-    class_out_test = predict(lda,X(train_len+1:end,:));
-    [test_lda_CM,test_grpOrder] = confusionmat(Y(train_len+1:end),class_out_test);
+%     % 1.4 Apply model on Testing Set
+    Y_pred_test = sim(net,X(train_len+1:end,:)');
+    Y_pred_test(Y_pred_test > 1.5) = 2;
+    Y_pred_test(Y_pred_test < 1.5) = 1;
+
+     [test_ann_CM,test_grpOrder] = confusionmat(Y(train_len+1:end,:),Y_pred_test);
+     
     if size(test_lda_CM,1) == 1
         correrate_test = correrate_test + 1;
     else
-        correrate_test = correrate_test + (test_lda_CM(1,1) + test_lda_CM(2,2))/test_len;
+        correrate_test = correrate_test + (test_ann_CM(1,1) + test_ann_CM(2,2))/test_len;
     end
 end
 
-lda_avg_cr_train = correrate_train / 10;
-lda_avg_cr_test = correrate_test / 10;
+ann_avg_cr_train = correrate_train / 10;
+ann_avg_cr_test = correrate_test / 10;
